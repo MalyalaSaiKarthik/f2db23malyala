cae626aad182d48fed63a19a2712b701dc9f3115
Sai Karthik Malyala <108293822+MalyalaSaiKarthik@users.noreply.github.com>
2022-11-06 22:34:43 -0600

    Updated app to work with mongodb

---
+++ models/motels.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose")
+
+const motelsSchema = mongoose.Schema({
+    motelRatings: String,
+    motelCost: { type: Number, min: 100, max: 10000 },
+    motelFacility: { type: String, minLength: 2 }
+})
+
+module.exports = mongoose.model("motels", motelsSchema)
