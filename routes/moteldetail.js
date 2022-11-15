9fa141df6cb720edff153151176cce51a8441e7d
Sai Karthik Malyala <108293822+MalyalaSaiKarthik@users.noreply.github.com>
2022-11-15 13:14:56 -0600

    Deleting unwanted files

--- routes/moteldetail.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-const motels_controlers = require('../controllers/motels');
-var router = express.Router();
-
-/* GET detail costume page */
-router.get('/detail', motels_controlers.motels_view_one_Page);
-
-module.exports = router;
