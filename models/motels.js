42171d05bd7a50d26e5a9b80f74ddf8a081c4c73
Sai Karthik Malyala <108293822+MalyalaSaiKarthik@users.noreply.github.com>
2022-11-29 16:35:38 -0600

    Added validation to motelCost

--- models/motels.js
+++ models/motels.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose")
 const motelsSchema = mongoose.Schema({
     motelRatings: String,
     motelCost: { type: Number, min: 100, max: 10000 },
-    motelFacility: { type: String, minLength: 2 }
+    motelFacility: { type: String }
 })
 
 module.exports = mongoose.model("motels", motelsSchema)
