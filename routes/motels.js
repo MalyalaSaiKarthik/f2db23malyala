cf6d4d01beab287b8f7fb91582f46bf9939db040
Sai Karthik Malyala <108293822+MalyalaSaiKarthik@users.noreply.github.com>
2022-11-29 16:54:54 -0600

    Added validation to collection and secured create and delete pages

--- routes/motels.js
+++ routes/motels.js
@@ -18,10 +18,10 @@ router.get('/', motels_controlers.motels_view_all_Page);
 /* GET detail motel page */
 router.get('/detail', motels_controlers.motels_view_one_Page);
 /* GET create costume page */
-router.get('/create', motels_controlers.motels_create_Page);
+router.get('/create', secured, motels_controlers.motels_create_Page);
 /* GET create update page */
 router.get('/update', secured, motels_controlers.motels_update_Page);
 /* GET delete costume page */
-router.get('/delete', motels_controlers.motels_delete_Page);
+router.get('/delete', secured, motels_controlers.motels_delete_Page);
 
 module.exports = router;
